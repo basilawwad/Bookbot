a25640a40bfa938063e4c66e7023d231d5d4c280
basilawwad <bawwad032@gmail.com>
2024-09-07 07:40:51 +0300

    print words

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_count = count_chars(text)
     print(f"{num_words} words found in the document")
+    print(char_count)
 
 
 def get_num_words(text):
@@ -14,5 +16,14 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_chars(text):
+    joined_text = text.replace(' ', '').lower()
+    text_map = dict()
+    for char in joined_text:
+        if char in text_map:
+            text_map[char] = text_map[char] + 1
+        else:
+            text_map[char] = 1
+    return text_map
 
 main()

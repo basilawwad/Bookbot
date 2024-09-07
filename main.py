3b9a5a8d4a16ba716b79cd044a4545780b44aa15
basilawwad <bawwad032@gmail.com>
2024-09-07 07:30:32 +0300

    count

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hello world!')
+book_path = 'books/frankenstein.txt'
+book_open = open(book_path, "r")
+book_content = book_open.read()
+split_book_content = book_content.split()
+word_count = len(split_book_content)
+print(word_count)
